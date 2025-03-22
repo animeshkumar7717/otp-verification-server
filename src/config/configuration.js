28d9e1db6e7265227569cd072b4c601a7eb84743
Animesh Kumar <animesh.kumar@applore.in>
2025-03-22 20:05:37 +0530

    change the mongoUrl to the config file

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const configuration = {
-    PORT : process.env.PORT || 5000
+    PORT : process.env.PORT || 5000,
+    mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/otp-verification' 
 }
 
 module.exports = configuration;
--- src/libs/Connection.js
+++ src/libs/Connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const configuration = require('../config/configuration');
 require('dotenv').config()
 
 const Connection = () => {
-    mongoose.connect(process.env.MONGO_URI).then((data)=>{
+    mongoose.connect(configuration.mongoUri).then((data)=>{
         console.log('successfully connected with database', data.connection.host);
     }).catch(err=>{
         console.log('failed to connect with the database: ', err);
