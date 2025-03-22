c32f7030570f6efa8ac74087337edfa4cf5a2c9d
Animesh Kumar <animesh.kumar@applore.in>
2025-03-22 12:25:42 +0530

    check conflict

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,6 @@ const { sendOTP, verifyOTP } = require("../controller/authController");
 const router = express.Router();
 
 router.post("/send-otp", sendOTP);
-router.post("/verify-otp", verifyOTP);
+router.post("/verification", verifyOTP);
 
 module.exports = router;
