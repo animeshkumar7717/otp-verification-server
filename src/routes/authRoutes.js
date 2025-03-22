6f1decf1e318434302fd7bea6acf2c7eb41db2a6
Animesh Kumar <63787396+animeshkumar7717@users.noreply.github.com>
2025-03-22 12:29:32 +0530

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,6 +4,6 @@ const { sendOTP, verifyOTP } = require("../controller/authController");
 const router = express.Router();
 
 router.post("/send-otp", sendOTP);
-router.post("/verification", verifyOTP);
+router.post("/verify-otp", verifyOTP);
 
 module.exports = router;
